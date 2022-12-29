c9cf26c7aa476844594bb2ac9de34d794ca1ce58
jabar.latif <jabar.latif@tiendeo.com>
2022-12-29 19:46:14 +0100

    agregado fecha y fucionando

--- dashboard/src/scenes/bar/index.jsx
+++ dashboard/src/scenes/bar/index.jsx
@@ -3,8 +3,16 @@ import Header from "../../components/Header";
 import BarChart from "../../components/BarChart";
 import { powerConsumption as power } from "../../data/mockData";
 import PowerChart from "../../components/LineCharts/PowerConsumtion";
+import React, {useState} from "react";
 
 const Bar = () => {
+  const [date, setDate] = useState("");
+
+  function handleChange(event) {
+    setDate(event.target.value);
+    console.log(date);
+  }
+
   console.log(power);
   return (
     <Box m="20px">
@@ -12,8 +20,11 @@ const Bar = () => {
       <Box height="75vh">
         <PowerChart data={power} />
       </Box>
-      <label for="birthday">Birthday:</label>
-      <input type="date" id="birthday" name="birthday"></input>
+      {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"></link> */}
+      <form>
+        <label htmlFor="birthday">Birthday:</label>
+        <input type="date" id="birthday" name="birthday" onChange={handleChange}></input>
+      </form>
     </Box>
   );
 };
